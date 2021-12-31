fc4f268bf5c72b9143207980c8049bb0be08490c
German Cocca <coccagerman@gmail.com>
2021-12-31 11:12:44 -0300

    test

--- app.js
+++ app.js
@@ -17,15 +17,6 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose.'))
 
-/* CORS config */
-const whitelist = ['http://localhost:3000', 'https://helpr-front.vercel.app/']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) callback(null, true)
-    else callback(new Error('Not allowed by CORS'))
-  },
-  credentials: true,
-}
 /* Import modules */
 const cors = require('cors')
 const session = require('express-session')
@@ -33,7 +24,7 @@ const passport = require('passport')
 const MongoDbStore = require('connect-mongo')
 
 /* Global middlewares */
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended: false}))
 app.use(session({
